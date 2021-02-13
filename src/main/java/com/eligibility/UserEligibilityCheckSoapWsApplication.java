ccdea9b55aa76567e9464f2734a774272ad4ce63
Ghanshyam Ribadiya <ghanshyam.ribadiya@getinsured.com>
2021-02-13 21:40:49 +0530

    initial project setup

---
+++ src/main/java/com/eligibility/UserEligibilityCheckSoapWsApplication.java
@@ -0,0 +1,13 @@
+package com.eligibility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserEligibilityCheckSoapWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserEligibilityCheckSoapWsApplication.class, args);
+	}
+
+}
